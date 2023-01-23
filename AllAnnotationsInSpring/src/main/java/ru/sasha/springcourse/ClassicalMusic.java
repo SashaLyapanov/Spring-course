a16803d34f21ffd30446b745eaf1858b650c8b9c
SashaLyapanov <99658299+SashaLyapanov@users.noreply.github.com>
2023-01-23 21:11:40 +0300

    Add files via upload

---
+++ AllAnnotationsInSpring/src/main/java/ru/sasha/springcourse/ClassicalMusic.java
@@ -0,0 +1,36 @@
+package ru.sasha.springcourse;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+@Component
+@Scope("prototype")
+public class ClassicalMusic implements Music{
+    List<String> classicalList = new ArrayList<>(Arrays.asList("First classical song", "Second classical song", "Third classical song"));
+
+    //пока будет стоять scope("prototype") данный Init метод не будет отрабатывать!
+    @PostConstruct
+    public void doMuInit() {
+        System.out.println("Doing my initialization");
+    }
+
+    @PreDestroy
+    public void doMyDestroy() {
+        System.out.println("Doing my destruction");
+    }
+
+
+    public String getSong() {
+        Random random = new Random();
+        int randomNum = random.nextInt(3);
+        return classicalList.get(randomNum);
+    }
+}
