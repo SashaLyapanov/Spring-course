0462e2c098f2f5dc6f3fd88b3dbeaed077f44788
SashaLyapanov <99658299+SashaLyapanov@users.noreply.github.com>
2023-01-23 14:42:47 +0300

    Delete FirstSpringApplication directory

--- FirstSpringApplication/src/main/java/ru/sasha/springcourse/JazzMusic.java
+++
@@ -1,7 +0,0 @@
-package ru.sasha.springcourse;
-
-public class JazzMusic implements Music{
-    public String getSong() {
-        return "Some Jazz song";
-    }
-}
--- FirstSpringApplication/src/main/java/ru/sasha/springcourse/RockMusic.java
+++
@@ -1,7 +0,0 @@
-package ru.sasha.springcourse;
-
-public class RockMusic implements Music{
-    public String getSong() {
-        return "Some Rock song";
-    }
-}
