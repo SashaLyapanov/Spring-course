805093a63c20c374a24e330aeb0a2f2213ff5682
SashaLyapanov <99658299+SashaLyapanov@users.noreply.github.com>
2023-01-27 11:02:31 +0300

    Add files via upload

---
+++ CRUDApplication/src/main/java/ru/sasha/springcourse/controllers/PeopleController.java
@@ -0,0 +1,36 @@
+package ru.sasha.springcourse.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.sasha.springcourse.dao.PersonDAO;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+
+    private final  PersonDAO personDAO;
+    @Autowired
+    public PeopleController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+
+    @GetMapping()
+    public String index(Model model) {
+        model.addAttribute("people", personDAO.index());
+        return "people/index";
+    }
+
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model){
+        //Получим конкретного человека из DAO по id и передадим в view
+        model.addAttribute("person", personDAO.show(id));
+        return "people/person";
+    }
+
+}
