155ded67182a36223c800f06b464c33f6b89e989
SashaLyapanov <99658299+SashaLyapanov@users.noreply.github.com>
2023-01-31 14:01:06 +0300

    Add files via upload

--- CRUDApplication/src/main/java/ru/sasha/springcourse/dao/PersonDAO.java
+++ CRUDApplication/src/main/java/ru/sasha/springcourse/dao/PersonDAO.java
@@ -1,5 +1,8 @@
 package ru.sasha.springcourse.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import ru.sasha.springcourse.models.Person;
 import org.springframework.stereotype.Component;
 
@@ -10,123 +13,42 @@ import java.util.List;
 
 @Component
 public class PersonDAO {
-    //На первой стадии не используем БД, а просто генерируем список, т.к. быстрее понять суть и легче (без SQL).
-    //Список людей
-    private static int PEOPLE_COUNT = 0;
 
-    private static final String URL = "jdbc:postgresql://localhost:5432/FirstDB";
-    private static final String PASSWORD = "28jrn2002$";
-    private static final String USERNAME = "postgres";
-
-    private static Connection connection;
-
-    static {
-        try{
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        try{
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+    private final JdbcTemplate jdbcTemplate;
+    @Autowired
+    public PersonDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
 
-
     //Метод, возвращающий всех людей из БД
-    public List<Person> index() throws SQLException {
-        List<Person> people = new ArrayList<>();
-
-        Statement statement = connection.createStatement();
-        String SQL = "SELECT * FROM Person";
-        ResultSet resultSet = statement.executeQuery(SQL);
-
-        while(resultSet.next()) {
-            Person person = new Person();
-
-            person.setId(resultSet.getInt("id"));
-            person.setName(resultSet.getString("name"));
-            person.setSurname(resultSet.getString("surname"));
-            person.setAge(resultSet.getInt("age"));
-            person.setEmail(resultSet.getString("email"));
-
-            people.add(person);
-        }
-        return people;
+    public List<Person> index() {
+        return jdbcTemplate.query("SELECT * FROM Person", new BeanPropertyRowMapper<>(Person.class));
     }
 
     //Метод, возвращающий чела по ID
     public Person show(int id) {
-        //Обязатлеьно объявить переменную Person person = null вне блока try, т.к. при return person
-        //у нас переменная, объявленная в блоке try не будет видна вне этого блока!!!
-        Person person = null;
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM Person WHERE id = ?");
-            preparedStatement.setInt(1, id);
-
-            ResultSet resultSet = preparedStatement.executeQuery();
-
-            resultSet.next();
-
-            person = new Person();
-            person.setId(resultSet.getInt("id"));
-            person.setName(resultSet.getString("name"));
-            person.setSurname(resultSet.getString("surname"));
-            person.setAge(resultSet.getInt("age"));
-            person.setEmail(resultSet.getString("email"));
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return person;
+        return jdbcTemplate.query("SELECT * FROM Person WHERE id = ?", new Object[]{id}, new PersonMapper())
+                .stream().findAny().orElse(null);
     }
 
 
     //Метод добавления человека в список
     public void save(Person person) {
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO Person VALUES(1, ?, ?, ?, ?)");
+        // можно так написать, т.к. метод update поддерживает внесение множества аргументов, которые будут рассмотрены, как массив данных, которые неободимо вставить
+        jdbcTemplate.update("INSERT INTO Person VALUES(1, ?, ?, ?, ?)", person.getName(), person.getSurname(), person.getAge(), person.getEmail());
 
-            preparedStatement.setString(1, person.getName());
-            preparedStatement.setString(2, person.getSurname());
-            preparedStatement.setInt(3, person.getAge());
-            preparedStatement.setString(4, person.getEmail());
-
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 
     //Метод для изменения данных человека
     public void update(int id, Person updatePerson) {
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement("UPDATE Person SET name = ?, surname = ?, age = ?, email = ? WHERE id = ?");
-
-            preparedStatement.setString(1, updatePerson.getName());
-            preparedStatement.setString(2, updatePerson.getSurname());
-            preparedStatement.setInt(3, updatePerson.getAge());
-            preparedStatement.setString(4, updatePerson.getEmail());
-            preparedStatement.setInt(5, id);
-
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        jdbcTemplate.update("UPDATE Person SET name=?, surname=?, age=?, email=? WHERE id=?", updatePerson.getName(), updatePerson.getSurname(),
+                updatePerson.getAge(), updatePerson.getEmail(), id);
     }
 
 
     //Метод удаления человека
     public void delete(int id) {
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM Person where id = ?");
-            preparedStatement.setInt(1, id);
-            preparedStatement.executeUpdate();
-        } catch(SQLException e) {
-            e.printStackTrace();
-        }
+        jdbcTemplate.update("DELETE FROM Person WHERE id=?", id);
     }
 }
